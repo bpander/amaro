9496c4582a18357b920719628980beedeb4439a0
Brad Anderson <brad.anderson87@gmail.com>
2016-03-30 21:38:33 -0500

    Adds a basic amdclean + uglify grunt setup.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+    var fs = require('fs');
+    var amdclean = require('amdclean');
+
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // pkg: grunt.file.readJSON('package.json'),
+
+        requirejs: {
+            dist: {
+                options: {
+                    findNestedDependencies: true,
+                    baseUrl: 'src',
+                    optimize: 'none',
+                    include: ['Lib.js'],
+                    out: 'State.js',
+                    wrap: {
+                        start: '',
+                        end: ''
+                    },
+                    onModuleBundleComplete: function (data) {
+                        var outputFile = data.path;
+
+                        fs.writeFileSync(outputFile, amdclean.clean({
+                            filePath: outputFile,
+                            // wrap: {
+                            //     start: fs.readFileSync('src/start.js.frag', { encoding: 'utf8' }),
+                            //     end: fs.readFileSync('src/end.js.frag', { encoding: 'utf8' })
+                            // }
+                        }));
+                    }
+                }
+            }
+        },
+
+
+        uglify: {
+            dist: {
+                files: {
+                    'State.min.js': ['State.js']
+                }
+            }
+        }
+
+    });
+
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+    // Define tasks
+    grunt.registerTask('default', ['requirejs:dist', 'uglify:dist']);
+
+};

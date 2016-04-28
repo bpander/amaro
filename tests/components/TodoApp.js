d0f4adaa2b665441732792d0a6571635717548f4
Brad Anderson <brad.anderson87@gmail.com>
2016-04-27 21:43:44 -0500

    Adds Promise, Object.assign, and classList polyfills in 'components' example for IE10. Adds a preventDefault for some real weird IE10 shit.

--- tests/components/TodoApp.js
+++ tests/components/TodoApp.js
@@ -17,10 +17,11 @@ define(function (require) {
     TodoApp.defaults = {
         todos: [],
         onkeydown: function (e) {
-            if (e.keyCode === 13) {
+            if (e.keyCode === 13 && e.target.value.trim() !== '') {
                 this.state.todos.push({ text: e.target.value, id: _id++ });
                 this.setState();
                 e.target.value = '';
+                e.preventDefault(); // IE does some real weird stuff on enter press
             }
         },
         removeTodo: function (todo) {
